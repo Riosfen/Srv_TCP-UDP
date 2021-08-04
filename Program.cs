03e618aabcd6ad2944467eda54485183a806c458
Victor Manuel Sanchez Mora <Riosfen@gmail.com>
2021-08-04 18:42:49 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Servidor_TCP
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        
+            String WorkingDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            Console.WriteLine("¡Arrancando servidor TCP!");
+
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(WorkingDirectory + "/Server.config");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //split linea y trim (guardar en un array clave valor)
+                    if (line.Contains('='))
+                    {
+                        ///TODO enviar a una clase que se encargue de setear las variables
+                        List<string> parts = line.Split('=').Select(p => p.Trim()).ToList();
+                    }
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
---
+++ base/MyTcpListener.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MyTcpListener
+{
+    public MyTcpListener()
+    {
+        TcpListener server = null;
+        try
+        {
+            ///TODO Configuracion a través de un archivo clave-valor.
+            /* configuración que tendrá este archivo:
+             * - Puerto 
+             * - Nombre Servidor
+             * - Numero maximo de conexiones (especificar un maximo por codigo)
+             * - Puerto aplicacion web
+             * - Tiempo maximo desconexión automatica
+             * - Intentos de reconexion
+             * - Intentos de reconexion maximos
+             * - Ping maximo
+             * - etc
+             */
+            ///TODO Pagina web que controle y monitorice el servidor (web porque se puede ejecuar en cualquier SO y se puede enviar por la red).
+            /* 
+             *
+             */
+            ///TODO API para mostrar datos generales de la sesión en paginas web.
+            /* 
+             * 
+             */
+
+
+            // Set the TcpListener on port 13000.
+            Int32 port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests.
+            server.Start();
+
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            // Enter the listening loop.
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also use server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+
+                // Shutdown and end connection
+                client.Close();
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
+    }
+}
